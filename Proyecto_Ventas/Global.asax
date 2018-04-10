0ef211c39cef88bca4c0d9e9166be1916a00a5ea
Ludswing Michael Cahuec Ojom <ludswingc@gmail.com>
2018-04-09 20:54:52 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Ventas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Ventas.MvcApplication" Language="C#" %>
---
+++ Proyecto_Ventas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Ventas.Startup))]
+namespace Proyecto_Ventas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
